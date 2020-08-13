61a7a51a7709aefcf75ad4771db764bc1cc0659f
DiabateJS <diabatejs@yahoo.fr>
2020-08-13 04:45:35 +0100

    Intégration de bootstrap et création du formulaire authentification.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UserAuthComponent} from './user-auth/user-auth.component';
+import {AppComponent} from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: UserAuthComponent
+  },
+  {
+    path: '',
+    component: AppComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
